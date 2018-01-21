b252de69d22909594ab805bb09cdb903133537a5
suconghou <1126045770@qq.com>
2018-01-21 15:29:18 +0800

    up

--- main.go
+++ main.go
@@ -11,10 +11,6 @@ import (
 	"github.com/suconghou/libm3u8"
 )
 
-// Data  struct {
-// 	Hls_url string
-// }
-
 type resJSON struct {
 	Error int
 	Data  interface{}
@@ -22,6 +18,28 @@ type resJSON struct {
 
 func main() {
 	id := os.Args[1]
+	if len(id) > 12 {
+		playVideo(id)
+		return
+	}
+	playLive(id)
+}
+
+func playVideo(vid string) {
+	url, err := getVideoM3U8URL(vid)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	m, err := libm3u8.NewFromURL(url, nil)
+	if err == nil {
+		io.Copy(os.Stdout, m.Play())
+	} else {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func playLive(id string) {
 	url, err := getM3u8URL(id)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -73,6 +91,31 @@ func getM3u8URL(id string) (string, error) {
 		data := ret.Data.(map[string]interface{})
 		if data["hls_url"] != nil {
 			return data["hls_url"].(string), nil
+		}
+	}
+	return "", fmt.Errorf(ret.Data.(string))
+}
+
+func getVideoM3U8URL(vid string) (string, error) {
+	u := fmt.Sprintf("https://vmobile.douyu.com/video/getInfo?vid=%s", vid)
+	resp, err := http.Get(u)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	bodyByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	var ret resJSON
+	err = json.Unmarshal(bodyByte, &ret)
+	if err != nil {
+		return "", err
+	}
+	if ret.Error == 0 {
+		data := ret.Data.(map[string]interface{})
+		if data["video_url"] != nil {
+			return data["video_url"].(string), nil
 		}
 	}
 	return "", fmt.Errorf(ret.Data.(string))

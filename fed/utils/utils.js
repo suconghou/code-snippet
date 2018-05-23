b5bfc6bba16decb89a445ed853d60a618a855dca
suconghou <1126045770@qq.com>
2018-05-23 20:45:04 +0800

    update

--- fed/utils/utils.js
+++ fed/utils/utils.js
@@ -284,6 +284,50 @@ function throttle2(fn, wait) {
   }
 }
 
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+}
+
+Array.prototype.unique = function () {
+  const set = new Set(this);
+  return Array.from(set);
+}
+
+Array.prototype.unique = function () {
+  const tmp = new Map();
+  return this.filter(item => {
+    return !tmp.has(item) && tmp.set(item, 1);
+  })
+}
+// https://juejin.im/post/5b0284ac51882542ad774c45
+
+
+/**
+ * Composes single-argument functions from right to left. The rightmost
+ * function can take multiple arguments as it provides the signature for
+ * the resulting composite function.
+ *
+ * @param {...Function} funcs The functions to compose.
+ * @returns {Function} A function obtained by composing the argument functions
+ * from right to left. For example, compose(f, g, h) is identical to doing
+ * (...args) => f(g(h(...args))).
+ */
+
+export default function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+
+
+
 /**
  * 获取url参数
  * @param {string} url - url地址

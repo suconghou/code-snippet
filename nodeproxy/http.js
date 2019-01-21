f1ca762c59d7e7b00b0f0769f3f01aaa571465e9
suconghou <1126045770@qq.com>
2019-01-21 17:09:46 +0800

    update node proxy http

--- nodeproxy/http.js
+++ nodeproxy/http.js
@@ -1,20 +1,26 @@
-var http = require('http');
-var proxy = http.createServer(function(request, response) {
-    var headers = Object.assign({}, request.headers, {});
-    delete headers.host;
-    var options = {
-        hostname: 'vvv.suconghou.cn',
-        method: request.method,
-        headers: headers,
-        path: request.url
-    };
-    var req = http.request(options, function(res) {
-        response.writeHead(res.statusCode, res.headers);
-        res.pipe(response);
-    });
-    request.pipe(req);
-    req.on('error', function(error) {
-        console.info(error);
-    });
-    req.end();
-}).listen(18080);
+const http = require("http");
+const proxy = http
+	.createServer(function(request, response) {
+		fwd(request, response);
+	})
+	.listen(9090);
+
+const fwd = (request, response) => {
+	const headers = request.headers;
+	delete headers.host;
+	const options = {
+		hostname: "git.ourwill.cn",
+		method: request.method,
+		headers: headers,
+		path: request.url
+	};
+	var req = http.request(options, res => {
+		response.writeHead(res.statusCode, res.headers);
+		res.pipe(response);
+	});
+	request.pipe(req);
+	req.on("error", error => {
+		console.info(error);
+		response.end();
+	});
+};

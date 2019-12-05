9a50c4da97689868e224c3bd01efabae22c77e30
suconghou <1126045770@qq.com>
2019-12-05 18:12:21 +0800

    fix: img check

--- php-benchmark/php-mime/mime.php
+++ php-benchmark/php-mime/mime.php
@@ -1,6 +1,11 @@
 
 <?php
 
+$png = "1.png.docx";
+$jpg = "1.jpg.gz";
+$gif = "1.gif.zip";
+
+
 $png = "1.png";
 $jpg = "1.jpg";
 $gif = "1.gif";
@@ -11,49 +16,69 @@ file_get_contents($png);
 file_get_contents($jpg);
 file_get_contents($gif);
 
-function minimime($fname) {
-    $fh=fopen($fname,'rb');
-    if ($fh) { 
-        $bytes6=fread($fh,6);
-        fclose($fh); 
-        if ($bytes6===false) return false;
-        if (substr($bytes6,0,3)=="\xff\xd8\xff") return 'image/jpeg';
-        if ($bytes6=="\x89PNG\x0d\x0a") return 'image/png';
-        if ($bytes6=="GIF87a" || $bytes6=="GIF89a") return 'image/gif';
+function minimime($fname)
+{
+    $fh = fopen($fname, 'rb');
+    if ($fh) {
+        $bytes6 = fread($fh, 6);
+        fclose($fh);
+        if ($bytes6 === false) return false;
+        if (substr($bytes6, 0, 3) == "\xff\xd8\xff") return 'image/jpeg';
+        if ($bytes6 == "\x89PNG\x0d\x0a") return 'image/png';
+        if ($bytes6 == "GIF87a" || $bytes6 == "GIF89a") return 'image/gif';
         return 'application/octet-stream';
     }
     return false;
 }
 
-$time = microtime( true );
-for( $i = 200; $i--; ) {
+
+function minimime_($fname)
+{
+    $bytes6 = file_get_contents($fname,false,null,0,6);
+    if ($bytes6) {
+        if ($bytes6 === false) return false;
+        if (substr($bytes6, 0, 3) == "\xff\xd8\xff") return 'image/jpeg';
+        if ($bytes6 == "\x89PNG\x0d\x0a") return 'image/png';
+        if ($bytes6 == "GIF87a" || $bytes6 == "GIF89a") return 'image/gif';
+        return 'application/octet-stream';
+    }
+    return false;
+}
+
+
+$time = microtime(true);
+for ($i = 200; $i--;) {
     getimagesize($png);
     getimagesize($jpg);
-    getimagesize($gif);
+    $r = getimagesize($gif);
 }
-echo( "getimagesize took " . (microtime( true ) - $time) . " seconds.".PHP_EOL );
+var_dump($r);
+echo ("getimagesize took " . (microtime(true) - $time) . " seconds." . PHP_EOL);
 
-$time = microtime( true );
-for( $i = 200; $i--; ) {
+$time = microtime(true);
+for ($i = 200; $i--;) {
     exif_imagetype($png);
     exif_imagetype($jpg);
-    exif_imagetype($gif);
+    $r = exif_imagetype($gif);
 }
-echo( "exif_imagetype took " . (microtime( true ) - $time) . " seconds.".PHP_EOL );
+var_dump($r);
+echo ("exif_imagetype took " . (microtime(true) - $time) . " seconds." . PHP_EOL);
 
-$time = microtime( true );
-for( $i = 200; $i--; ) {
+$time = microtime(true);
+for ($i = 200; $i--;) {
     mime_content_type($png);
     mime_content_type($jpg);
-    mime_content_type($gif);
+    $r = mime_content_type($gif);
 }
-echo( "mime_content_type took " . (microtime( true ) - $time) . " seconds.".PHP_EOL );
+var_dump($r);
+echo ("mime_content_type took " . (microtime(true) - $time) . " seconds." . PHP_EOL);
 
 
-$time = microtime( true );
-for( $i = 200; $i--; ) {
+$time = microtime(true);
+for ($i = 200; $i--;) {
     minimime($png);
     minimime($jpg);
-    minimime($gif);
+    $r = minimime($gif);
 }
-echo( "minimime took " . (microtime( true ) - $time) . " seconds.".PHP_EOL );
+var_dump($r);
+echo ("minimime took " . (microtime(true) - $time) . " seconds." . PHP_EOL);
